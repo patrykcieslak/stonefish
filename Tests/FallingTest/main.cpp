b6b02ff229692fdbd5a5f7ccc47b4170e6081a9e
Patryk Cieslak <zbuffer@Patryks-MacBook-Air.local>
2014-03-02 23:11:43 +0100

    Created basic directory structure.

---
+++ Tests/FallingTest/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  FallingTest
+//
+//  Created by Patryk Cieslak on 02/03/2014.
+//  Copyright (c) 2014 Patryk Cieslak. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+    std::cout << "Falling objects test.\n";
+    return 0;
+}
+
