87dbc9864d22c2426b63168f969abed5a9c7976d
Michele Grimaldi <michelegrmld@gmail.com>
2024-03-27 18:24:06 +0000

    removed build

--- build/CMakeFiles/Stonefish.dir/3rdparty/LinearMath/btSerializer.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/Stonefish.dir/3rdparty/LinearMath/btSerializer.cpp.o: \
- /home/michele/Desktop/stonefish_mike/stonefish/3rdparty/LinearMath/btSerializer.cpp \
- /usr/include/stdc-predef.h
--- build/CMakeFiles/Stonefish.dir/3rdparty/LinearMath/btSerializer64.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/Stonefish.dir/3rdparty/LinearMath/btSerializer64.cpp.o: \
- /home/michele/Desktop/stonefish_mike/stonefish/3rdparty/LinearMath/btSerializer64.cpp \
- /usr/include/stdc-predef.h
